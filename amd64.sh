f9e6c568087998b86b45b54bd4a9182421d721d5
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-22 11:39:08 +0200

    Update amd64.sh

--- amd64.sh
+++ amd64.sh
@@ -4,9 +4,9 @@ wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-headers-5
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-headers-5.8.3-050803_5.8.3-050803.202008211236_all.deb
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-image-unsigned-5.8.3-050803-generic_5.8.3-050803.202008211236_amd64.deb
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-modules-5.8.3-050803-generic_5.8.3-050803.202008211236_amd64.deb
-sudo dpkg -i *.deb
 
-echo "Do you wish to install this program?"
+
+echo "do you want to reboot?"
 select yn in "y" "n"; do
     case $yn in
         y ) sudo reboot; break;;
