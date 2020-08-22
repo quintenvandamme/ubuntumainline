d3044b9d09d6e9140efab67e609a4b8718d61d0c
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-22 11:47:28 +0200

    Update amd64.sh

--- amd64.sh
+++ amd64.sh
@@ -4,7 +4,7 @@ wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-headers-5
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-headers-5.8.3-050803_5.8.3-050803.202008211236_all.deb
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-image-unsigned-5.8.3-050803-generic_5.8.3-050803.202008211236_amd64.deb
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-modules-5.8.3-050803-generic_5.8.3-050803.202008211236_amd64.deb
-
+sudo dpkg -i *.deb
 
 echo -n "do you want to reboot (y/n)? "
 old_stty_cfg=$(stty -g)
