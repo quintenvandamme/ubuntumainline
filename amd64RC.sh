862ad170f044019d94ac95e83b0717358180a42b
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-22 15:44:53 +0200

    fix wget

--- amd64RC.sh
+++ amd64RC.sh
@@ -3,7 +3,7 @@ cd ~/ubuntukernel5.9-rc1
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-headers-5.9.0-050900rc1-generic_5.9.0-050900rc1.202008162130_amd64.deb
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-headers-5.9.0-050900rc1_5.9.0-050900rc1.202008162130_all.deb
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-image-unsigned-5.9.0-050900rc1-generic_5.9.0-050900rc1.202008162130_amd64.deb
-whet https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-modules-5.9.0-050900rc1-generic_5.9.0-050900rc1.202008162130_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-modules-5.9.0-050900rc1-generic_5.9.0-050900rc1.202008162130_amd64.deb
 sudo dpkg -i *.deb
 
 echo -n "do you want to reboot (y/n)? "
