9f139e1dbfebe8ba032e000aa8ab21e39ce29f29
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-23 18:44:04 +0200

    add catalog copy

---
+++ catalog/5.8.3/amd64.sh
@@ -0,0 +1,19 @@
+# download kernel and kernel headers
+mkdir ~/ubuntukernel5.8.3
+cd ~/ubuntukernel5.8.3
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-headers-5.8.3-050803-generic_5.8.3-050803.202008211236_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-headers-5.8.3-050803_5.8.3-050803.202008211236_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-image-unsigned-5.8.3-050803-generic_5.8.3-050803.202008211236_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-modules-5.8.3-050803-generic_5.8.3-050803.202008211236_amd64.deb
+sudo dpkg -i *.deb
+cd
+rm -r ~/ubuntukernel5.8.3
+# y/n prompt
+echo -n "do you want to reboot (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg 
+if echo "$answer" | grep -iq "^y" ;then
+    sudo reboot
+else
+    exit
+fi
