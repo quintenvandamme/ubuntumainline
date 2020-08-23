b7e8acbc60c31fe875cd07714cdc689285e1a492
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-23 18:44:56 +0200

    add catalog copy

---
+++ catalog/5.9-rc1/amd64RC.sh
@@ -0,0 +1,19 @@
+# download linux and linux headers
+mkdir ~/ubuntukernel5.9-rc1
+cd ~/ubuntukernel5.9-rc1
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-headers-5.9.0-050900rc1-generic_5.9.0-050900rc1.202008162130_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-headers-5.9.0-050900rc1_5.9.0-050900rc1.202008162130_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-image-unsigned-5.9.0-050900rc1-generic_5.9.0-050900rc1.202008162130_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc1/amd64/linux-modules-5.9.0-050900rc1-generic_5.9.0-050900rc1.202008162130_amd64.deb
+sudo dpkg -i *.deb
+cd
+rm -r ~/ubuntukernel5.9-rc1
+# y/n prompt
+echo -n "do you want to reboot (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg 
+if echo "$answer" | grep -iq "^y" ;then
+    sudo reboot
+else
+    exit
+fi
