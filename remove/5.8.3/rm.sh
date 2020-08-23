14772bbb593b1b36ea2dd27c2041c8bdfdb51e93
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-23 12:02:28 +0200

    Create rm.sh

---
+++ remove/5.8.3/rm.sh
@@ -0,0 +1,12 @@
+echo "only remove kernel if you have a newer one!"
+
+echo -n "do you want ro remove linux 5.8.3 (y/n)? "
+old_stty_cfg=$(stty -g)
+stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg 
+if echo "$answer" | grep -iq "^y" ;then
+    sudo apt remove linux-headers-5.8.3-050803-generic
+    sudo apt remove linux-image-5.8.3-050803-generic
+    sudo apt remove linux-modules-5.8.3-050803-generic
+else
+    exit
+fi
