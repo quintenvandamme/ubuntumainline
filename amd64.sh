e2ef4a19e8394e602dafb23faa0c013b36495c50
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-24 16:09:14 +0200

    Update amd64.sh

--- amd64.sh
+++ amd64.sh
@@ -8,12 +8,3 @@ wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.3/amd64/linux-modules-5
 sudo dpkg -i *.deb
 cd
 rm -r ~/ubuntukernel5.8.3
-# y/n prompt
-echo -n "do you want to reboot (y/n)? "
-old_stty_cfg=$(stty -g)
-stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg 
-if echo "$answer" | grep -iq "^y" ;then
-    sudo reboot
-else
-    exit
-fi
