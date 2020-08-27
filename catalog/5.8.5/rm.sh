cc14563214a3bcbd93d504d6c3b2d7c679667237
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-08-27 14:21:59 +0200

    Add files via upload

---
+++ catalog/5.8.5/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.8.5-050805
+sudo apt remove linux-image-unsigned-5.8.5-050805-generic 
+sudo apt remove linux-modules-5.8.5-050805-generic
+echo linux 5.8.5 is successfully removed
