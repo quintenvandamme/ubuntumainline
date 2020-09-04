fa7ae53f273c8d8b37d4996a0cc68a455d9dbf0f
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-09-04 16:29:44 +0200

    5.8.6

--- amd64.sh
+++ amd64.sh
@@ -1,10 +1,5 @@
-# download linux and linux headers
-mkdir ~/ubuntukernel5.8.5
-cd ~/ubuntukernel5.8.5
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.5/amd64/linux-headers-5.8.5-050805-generic_5.8.5-050805.202008270831_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.5/amd64/linux-headers-5.8.5-050805_5.8.5-050805.202008270831_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.5/amd64/linux-image-unsigned-5.8.5-050805-generic_5.8.5-050805.202008270831_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.5/amd64/linux-modules-5.8.5-050805-generic_5.8.5-050805.202008270831_amd64.deb
-sudo dpkg -i *.deb
-cd
-rm -r ~/ubuntukernel5.8.5
+# download and installs linux and linux headers
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.6/amd64/linux-headers-5.8.6-050806-generic_5.8.6-050806.202009030635_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.6/amd64/linux-headers-5.8.6-050806_5.8.6-050806.202009030635_all.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.6/amd64/linux-image-unsigned-5.8.6-050806-generic_5.8.6-050806.202009030635_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.6/amd64/linux-modules-5.8.6-050806-generic_5.8.6-050806.202009030635_amd64.deb
