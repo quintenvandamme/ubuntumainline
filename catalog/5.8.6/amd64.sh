7d8dcf3dbd83d9f7ef0d19345d1823573364083a
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-09-04 16:29:08 +0200

    5.8.6

---
+++ catalog/5.8.6/amd64.sh
@@ -0,0 +1,5 @@
+# download and installs linux and linux headers
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.6/amd64/linux-headers-5.8.6-050806-generic_5.8.6-050806.202009030635_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.6/amd64/linux-headers-5.8.6-050806_5.8.6-050806.202009030635_all.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.6/amd64/linux-image-unsigned-5.8.6-050806-generic_5.8.6-050806.202009030635_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.6/amd64/linux-modules-5.8.6-050806-generic_5.8.6-050806.202009030635_amd64.deb
---
+++ catalog/5.8.6/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.8.6-050806
+sudo apt remove linux-image-unsigned-5.8.6-050806-generic 
+sudo apt remove linux-modules-5.8.6-050806-generic
+echo linux 5.8.6 is successfully removed
