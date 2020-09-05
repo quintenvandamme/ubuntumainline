9c8ed1b30b421bbfa51b05019aa6f4ba9884fd81
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-09-05 16:45:45 +0200

    5.8.7

---
+++ catalog/5.8.7/amd64.sh
@@ -0,0 +1,5 @@
+# download and installs linux and linux headers
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.7/amd64/linux-headers-5.8.7-050807-generic_5.8.7-050807.202009051031_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.7/amd64/linux-headers-5.8.7-050807_5.8.7-050807.202009051031_all.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.7/amd64/linux-image-unsigned-5.8.7-050807-generic_5.8.7-050807.202009051031_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.7/amd64/linux-modules-5.8.7-050807-generic_5.8.7-050807.202009051031_amd64.deb
