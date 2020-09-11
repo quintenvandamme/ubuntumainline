a920fd6198d817de54f0864ca55f4f7cbfedcd88
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-09-11 17:18:05 +0200

    5.8.8

--- amd64.sh
+++ amd64.sh
@@ -1,5 +1,5 @@
 # download and installs linux and linux headers
-sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.7/amd64/linux-headers-5.8.7-050807-generic_5.8.7-050807.202009051031_amd64.deb
-sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.7/amd64/linux-headers-5.8.7-050807_5.8.7-050807.202009051031_all.deb
-sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.7/amd64/linux-image-unsigned-5.8.7-050807-generic_5.8.7-050807.202009051031_amd64.deb
-sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.7/amd64/linux-modules-5.8.7-050807-generic_5.8.7-050807.202009051031_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.8/amd64/linux-headers-5.8.8-050808-generic_5.8.8-050808.202009091435_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.8/amd64/linux-headers-5.8.8-050808_5.8.8-050808.202009091435_all.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.8/amd64/linux-image-unsigned-5.8.8-050808-generic_5.8.8-050808.202009091435_amd64.deb
+sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.8/amd64/linux-modules-5.8.8-050808-generic_5.8.8-050808.202009091435_amd64.deb
