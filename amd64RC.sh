05caa44cfa17cdee32d3c20cec4130fdabd9d66c
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-09-14 17:04:25 +0200

    5.9-rc5

--- amd64RC.sh
+++ amd64RC.sh
@@ -1,10 +1,10 @@
 # download linux and linux headers
-mkdir ~/ubuntukernel5.9-rc4
-cd ~/ubuntukernel5.9-rc4
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc4/amd64/linux-headers-5.9.0-050900rc4-generic_5.9.0-050900rc4.202009070130_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc4/amd64/linux-headers-5.9.0-050900rc4_5.9.0-050900rc4.202009070130_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc4/amd64/linux-image-unsigned-5.9.0-050900rc4-generic_5.9.0-050900rc4.202009070130_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc4/amd64/linux-modules-5.9.0-050900rc4-generic_5.9.0-050900rc4.202009070130_amd64.deb
+mkdir ~/ubuntukernel5.9-rc5
+cd ~/ubuntukernel5.9-rc5
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc5/amd64/linux-headers-5.9.0-050900rc5-generic_5.9.0-050900rc5.202009141230_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc5/amd64/linux-headers-5.9.0-050900rc5_5.9.0-050900rc5.202009141230_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc5/amd64/linux-image-unsigned-5.9.0-050900rc5-generic_5.9.0-050900rc5.202009141230_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9-rc5/amd64/linux-modules-5.9.0-050900rc5-generic_5.9.0-050900rc5.202009141230_amd64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.9-rc4
+rm -r ~/ubuntukernel5.9-rc5
