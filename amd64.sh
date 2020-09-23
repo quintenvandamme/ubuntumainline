1ef11b312fe9e0a5f269c2682cb499d703be61ee
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-09-23 19:16:55 +0200

    5.8.11

--- amd64.sh
+++ amd64.sh
@@ -1,5 +1,10 @@
-# download and installs linux and linux headers
-sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.10/amd64/linux-headers-5.8.10-050810-generic_5.8.10-050810.202009171232_amd64.deb
-sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.10/amd64/linux-headers-5.8.10-050810_5.8.10-050810.202009171232_all.deb
-sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.10/amd64/linux-image-unsigned-5.8.10-050810-generic_5.8.10-050810.202009171232_amd64.deb
-sudo apt install ./https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.10/amd64/linux-modules-5.8.10-050810-generic_5.8.10-050810.202009171232_amd64.deb
+# download linux and linux headers
+mkdir ~/ubuntukernel5.8.11
+cd ~/ubuntukernel5.8.11
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.11/amd64/linux-headers-5.8.11-050811-generic_5.8.11-050811.202009230858_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.11/amd64/linux-headers-5.8.11-050811_5.8.11-050811.202009230858_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.11/amd64/linux-image-unsigned-5.8.11-050811-generic_5.8.11-050811.202009230858_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.11/amd64/linux-modules-5.8.11-050811-generic_5.8.11-050811.202009230858_amd64.deb
+sudo dpkg -i *.deb
+cd
+rm -r ~/ubuntukernel5.8.11
