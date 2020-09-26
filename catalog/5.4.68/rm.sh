f7f2e8c0f809bb3d6e87e735af0faa1403972dca
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-09-26 20:46:02 +0200

    5.4.68

---
+++ catalog/5.4.68/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.4.68-050468
+sudo apt remove linux-image-unsigned-5.4.68-050468-generic
+sudo apt remove linux-modules-5.4.68-050468-generic
+echo linux 5.4.68 is successfully removed
