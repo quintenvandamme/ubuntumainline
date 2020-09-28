d9acfd6d0b90da9edf5d0e1c86636e195ad6503e
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-09-28 09:38:28 +0200

    5.9-rc7

---
+++ catalog/5.9-rc7/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.9.0-050900rc7
+sudo apt remove linux-image-unsigned-5.9.0-050900rc7-generic
+sudo apt remove linux-modules-5.9.0-050900rc7-generic
+echo linux 5.9-rc7 is successfully removed
