c00ea8f3da2a2c84fa2984289f777c85c93acd4b
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-12 17:09:42 +0200

    5.9

--- arm64.sh
+++ arm64.sh
@@ -1,9 +1,9 @@
 # download linux and linux headers
-mkdir ~/ubuntukernel5.8.14
-cd ~/ubuntukernel5.8.14
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.14/arm64/linux-headers-5.8.14-050814-generic_5.8.14-050814.202010070730_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.14/arm64/linux-image-unsigned-5.8.14-050814-generic_5.8.14-050814.202010070730_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.14/arm64/linux-modules-5.8.14-050814-generic_5.8.14-050814.202010070730_arm64.deb
+mkdir ~/ubuntukernel5.9
+cd ~/ubuntukernel5.9
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/arm64/linux-headers-5.9.0-050900-generic_5.9.0-050900.202010112230_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/arm64/linux-image-unsigned-5.9.0-050900-generic_5.9.0-050900.202010112230_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/arm64/linux-modules-5.9.0-050900-generic_5.9.0-050900.202010112230_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.8.14
+rm -r ~/ubuntukernel5.9
