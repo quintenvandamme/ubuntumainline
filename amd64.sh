5f0b290fd5b4838c08e06eda4b05a63f8085f7ac
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-12 17:08:42 +0200

    5.9

--- amd64.sh
+++ amd64.sh
@@ -1,10 +1,10 @@
 # download linux and linux headers
-mkdir ~/ubuntukernel5.8.14
-cd ~/ubuntukernel5.8.14
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.14/amd64/linux-headers-5.8.14-050814-generic_5.8.14-050814.202010070730_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.14/amd64/linux-headers-5.8.14-050814_5.8.14-050814.202010070730_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.14/amd64/linux-image-unsigned-5.8.14-050814-generic_5.8.14-050814.202010070730_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.14/amd64/linux-modules-5.8.14-050814-generic_5.8.14-050814.202010070730_amd64.deb
+mkdir ~/ubuntukernel5.9
+cd ~/ubuntukernel5.9
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-headers-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-headers-5.9.0-050900_5.9.0-050900.202010112230_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-image-unsigned-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-modules-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.8.14
+rm -r ~/ubuntukernel5.9
