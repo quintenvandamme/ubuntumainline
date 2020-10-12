66834f6bd945144d5fda50b62ad1cc3b69c7f8f9
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-12 17:13:03 +0200

    5.9

---
+++ catalog/5.9/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.9.0-050900
+sudo apt remove linux-image-unsigned-5.9.0-050900-generic 
+sudo apt remove linux-modules-5.9.0-050900-generic
+echo linux 5.9 is successfully removed
