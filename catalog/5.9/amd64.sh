67b299f055d1d2a3ee04e0f4f44f48e2e8bd6fb5
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-12 17:11:23 +0200

    5.9

---
+++ catalog/5.9/amd64.sh
@@ -0,0 +1,10 @@
+# download linux and linux headers
+mkdir ~/ubuntukernel5.9
+cd ~/ubuntukernel5.9
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-headers-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-headers-5.9.0-050900_5.9.0-050900.202010112230_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-image-unsigned-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9/amd64/linux-modules-5.9.0-050900-generic_5.9.0-050900.202010112230_amd64.deb
+sudo dpkg -i *.deb
+cd
+rm -r ~/ubuntukernel5.9
