1790fe123ae696887a472d29aee3780d9962625c
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-14 12:52:48 +0200

    5.4.71

---
+++ catalog/5.4.71/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.4.71-050471
+sudo apt remove linux-image-unsigned-5.4.71-050471-generic
+sudo apt remove linux-modules-5.4.71-050471-generic
+echo linux 5.4.71 is successfully removed
