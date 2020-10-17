18065ea148cf6c2dc8c2c62f1f1b61ebfa7bd6a2
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-17 13:11:00 +0200

    5.4.72

--- arm64STABLE.sh
+++ arm64STABLE.sh
@@ -1,9 +1,9 @@
 # download linux and linux headers
-mkdir ~/ubuntukernel5.4.71
-cd ~/ubuntukernel5.4.71
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.71/arm64/linux-headers-5.4.71-050471-generic_5.4.71-050471.202010140934_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.71/arm64/linux-image-unsigned-5.4.71-050471-generic_5.4.71-050471.202010140934_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.71/arm64/linux-modules-5.4.71-050471-generic_5.4.71-050471.202010140934_arm64.deb
+mkdir ~/ubuntukernel5.4.72
+cd ~/ubuntukernel5.4.72
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/arm64/linux-headers-5.4.72-050472-generic_5.4.72-050472.202010170535_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/arm64/linux-image-unsigned-5.4.72-050472-generic_5.4.72-050472.202010170535_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/arm64/linux-modules-5.4.72-050472-generic_5.4.72-050472.202010170535_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.4.71
+rm -r ~/ubuntukernel5.4.72
