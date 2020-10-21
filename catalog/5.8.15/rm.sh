945ee60a742eb40ef9cca5b13f1bf045d13bb26b
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-21 17:25:49 +0200

    5.8.15

---
+++ catalog/5.8.15/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.8.15-0508015
+sudo apt remove linux-image-unsigned-5.8.15-0508015-generic 
+sudo apt remove linux-modules-5.8.15-0508015-generic
+echo linux 5.8.15 is successfully removed
