e88360aa8383edbcf3459c316b79631d8a49f6a4
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-29 14:16:20 +0100

    5.8.17

---
+++ catalog/5.8.17/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.8.17-0508017
+sudo apt remove linux-image-unsigned-5.8.17-0508017-generic 
+sudo apt remove linux-modules-5.8.17-0508017-generic
+echo linux 5.8.17 is successfully removed
