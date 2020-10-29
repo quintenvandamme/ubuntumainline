b636684d9444dfeda0106d8542b4c2a46381129b
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-29 14:05:29 +0100

    5.4.73

--- amd64STABLE.sh
+++ amd64STABLE.sh
@@ -1,10 +1,10 @@
 # download linux and linux headers
-mkdir ~/ubuntukernel5.4.72
-cd ~/ubuntukernel5.4.72
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/amd64/linux-headers-5.4.72-050472-generic_5.4.72-050472.202010170535_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/amd64/linux-headers-5.4.72-050472_5.4.72-050472.202010170535_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/amd64/linux-image-unsigned-5.4.72-050472-generic_5.4.72-050472.202010170535_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.72/amd64/linux-modules-5.4.72-050472-generic_5.4.72-050472.202010170535_amd64.deb
+mkdir ~/ubuntukernel5.4.73
+cd ~/ubuntukernel5.4.73
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/amd64/linux-headers-5.4.73-050473-generic_5.4.73-050473.202010291054_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/amd64/linux-headers-5.4.73-050473_5.4.73-050473.202010291054_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/amd64/linux-image-unsigned-5.4.73-050473-generic_5.4.73-050473.202010291054_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/amd64/linux-modules-5.4.73-050473-generic_5.4.73-050473.202010291054_amd64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.4.72
+rm -r ~/ubuntukernel5.4.73
