5784114f3f9b42e0c768a669349dba89ba5c1b50
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-29 14:04:07 +0100

    5.9.2

--- arm64.sh
+++ arm64.sh
@@ -1,9 +1,9 @@
 # download linux and linux headers
-mkdir ~/ubuntukernel5.9.1
-cd ~/ubuntukernel5.9.1
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.1/arm64/linux-headers-5.9.1-050901-generic_5.9.1-050901.202010170731_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.1/arm64/linux-image-unsigned-5.9.1-050901-generic_5.9.1-050901.202010170731_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.1/arm64/linux-modules-5.9.1-050901-generic_5.9.1-050901.202010170731_arm64.deb
+mkdir ~/ubuntukernel5.9.2
+cd ~/ubuntukernel5.9.2
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.2/arm64/linux-headers-5.9.2-050902-generic_5.9.2-050902.202010290646_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.2/arm64/linux-image-5.9.2-050902-generic_5.9.2-050902.202010290646_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.2/arm64/linux-modules-5.9.2-050902-generic_5.9.2-050902.202010290646_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.9.1
+rm -r ~/ubuntukernel5.9.2
