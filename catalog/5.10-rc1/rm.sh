bad3096956d0b2987839252023a7a2160d99cd72
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-10-29 17:47:17 +0100

    5.10-rc1

---
+++ catalog/5.10-rc1/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.10.0-051000rc1
+sudo apt remove linux-image-unsigned-5.10.0-051000rc1-generic
+sudo apt remove linux-modules-5.10.0-051000rc1-generic
+echo linux 5.10-rc1 is successfully removed
