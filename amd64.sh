ad3bb6e6cfd133c4b15af407a81c85deab7526d7
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-02 07:30:12 +0100

    5.9.3

--- amd64.sh
+++ amd64.sh
@@ -1,10 +1,10 @@
 # download linux and linux headers
-mkdir ~/ubuntukernel5.9.2
-cd ~/ubuntukernel5.9.2
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.2/amd64/linux-headers-5.9.2-050902-generic_5.9.2-050902.202010290646_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.2/amd64/linux-headers-5.9.2-050902_5.9.2-050902.202010290646_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.2/amd64/linux-image-unsigned-5.9.2-050902-generic_5.9.2-050902.202010290646_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.2/amd64/linux-modules-5.9.2-050902-generic_5.9.2-050902.202010290646_amd64.deb
+mkdir /tmp/ubuntukernel5.9.3
+cd /tmp/ubuntukernel5.9.3
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.3/amd64/linux-headers-5.9.3-050903-generic_5.9.3-050903.202011011237_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.3/amd64/linux-headers-5.9.3-050903_5.9.3-050903.202011011237_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.3/amd64/linux-image-unsigned-5.9.3-050903-generic_5.9.3-050903.202011011237_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.3/amd64/linux-modules-5.9.3-050903-generic_5.9.3-050903.202011011237_amd64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.9.2
+rm -r /tmp/ubuntukernel5.9.3
