08fb9758b0f0e3c999efa4c2fa0a88d572ce11df
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-02 07:33:36 +0100

    5.9.3

---
+++ catalog/5.9.3/rm.sh
@@ -0,0 +1,7 @@
+
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.9.3-050903
+sudo apt remove linux-image-unsigned-5.9.3-050903-generic 
+sudo apt remove linux-modules-5.9.3-050903-generic
+echo linux 5.9.3 is successfully removed
