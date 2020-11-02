1fb02095878b43d78bc7baa1ac8bc5c8523f7dfc
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-02 07:25:37 +0100

    5.10-rc2

--- arm64RC.sh
+++ arm64RC.sh
@@ -1,9 +1,9 @@
 # download linux and linux headers
-mkdir ~/ubuntukernel5.10-rc1
-cd ~/ubuntukernel5.10-rc1
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc1/arm64/linux-headers-5.10.0-051000rc1-generic_5.10.0-051000rc1.202010291359_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc1/arm64/linux-image-unsigned-5.10.0-051000rc1-generic_5.10.0-051000rc1.202010291359_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc1/arm64/linux-modules-5.10.0-051000rc1-generic_5.10.0-051000rc1.202010291359_arm64.deb
+mkdir /tmp/ubuntukernel5.10-rc2
+cd /tmp/ubuntukernel5.10-rc2
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc2/arm64/linux-headers-5.10.0-051000rc2-generic_5.10.0-051000rc2.202011012330_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc2/arm64/linux-image-unsigned-5.10.0-051000rc2-generic_5.10.0-051000rc2.202011012330_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc2/arm64/linux-modules-5.10.0-051000rc2-generic_5.10.0-051000rc2.202011012330_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.10-rc1
+rm -r /tmp/ubuntukernel5.10-rc2
