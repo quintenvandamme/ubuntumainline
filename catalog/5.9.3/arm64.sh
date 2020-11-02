ef3f2e15e4cc5de93ec76c70705d6b59bce14934
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-02 07:32:47 +0100

    5.9.3

---
+++ catalog/5.9.3/arm64.sh
@@ -0,0 +1,9 @@
+# downloads linux and linux headers
+mkdir /tmp/ubuntukernel5.9.3
+cd /tmp/ubuntukernel5.9.3
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.3/arm64/linux-headers-5.9.3-050903-generic_5.9.3-050903.202011011237_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.3/arm64/linux-image-unsigned-5.9.3-050903-generic_5.9.3-050903.202011011237_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.3/arm64/linux-modules-5.9.3-050903-generic_5.9.3-050903.202011011237_arm64.deb
+sudo dpkg -i *.deb
+cd
+rm -r /tmp/ubuntukernel5.9.3
