d4cff06f8f131fea0b45d4c6e7a306a2e5f23bf9
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-02 07:37:05 +0100

    5.4.74

--- arm64LTS.sh
+++ arm64LTS.sh
@@ -1,9 +1,9 @@
-# download linux and linux headers
-mkdir ~/ubuntukernel5.4.73
-cd ~/ubuntukernel5.4.73
+# downloads linux and linux headers
+mkdir /tmp/ubuntukernel5.4.74
+cd /tmp/ubuntukernel5.4.74
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/arm64/linux-headers-5.4.73-050473-generic_5.4.73-050473.202010291054_arm64.deb
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/arm64/linux-image-unsigned-5.4.73-050473-generic_5.4.73-050473.202010291054_arm64.deb
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/arm64/linux-modules-5.4.73-050473-generic_5.4.73-050473.202010291054_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r ~/ubuntukernel5.4.73
+rm -r /tmp/ubuntukernel5.4.74
