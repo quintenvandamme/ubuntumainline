7a740f7f62630eab74c0f3868279e550edd99f2e
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-06 08:37:16 +0100

    5.4.75

--- arm64LTS.sh
+++ arm64LTS.sh
@@ -1,9 +1,9 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.4.74
-cd /tmp/ubuntukernel5.4.74
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/arm64/linux-headers-5.4.73-050473-generic_5.4.73-050473.202010291054_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/arm64/linux-image-unsigned-5.4.73-050473-generic_5.4.73-050473.202010291054_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.73/arm64/linux-modules-5.4.73-050473-generic_5.4.73-050473.202010291054_arm64.deb
+mkdir /tmp/ubuntukernel5.4.75
+cd /tmp/ubuntukernel5.4.75
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.75/arm64/linux-headers-5.4.75-050475-generic_5.4.75-050475.202011051231_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.75/arm64/linux-image-unsigned-5.4.75-050475-generic_5.4.75-050475.202011051231_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.75/arm64/linux-modules-5.4.75-050475-generic_5.4.75-050475.202011051231_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.4.74
+rm -r /tmp/ubuntukernel5.4.75
