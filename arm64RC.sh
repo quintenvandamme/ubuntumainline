754cbf5392ffb87598c5f5569a7e579133094f28
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-09 09:02:37 +0100

    5.10-rc3

--- arm64RC.sh
+++ arm64RC.sh
@@ -1,4 +1,4 @@
-# download linux and linux headers
+# downloads linux and linux headers
 mkdir /tmp/ubuntukernel5.10-rc3
 cd /tmp/ubuntukernel5.10-rc3
 wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc3/arm64/linux-headers-5.10.0-051000rc3-generic_5.10.0-051000rc3.202011082030_arm64.deb
