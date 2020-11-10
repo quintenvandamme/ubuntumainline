a62ce074cdb1ebd68004d6a24746740c3bdba934
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-10 20:10:24 +0100

    5.4.76

---
+++ catalog/5.4.76/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.4.76-050476
+sudo apt remove linux-image-unsigned-5.4.76-050476-generic
+sudo apt remove linux-modules-5.4.76-050476-generic
+echo linux 5.4.75 is successfully removed
