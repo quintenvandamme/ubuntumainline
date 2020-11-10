f0c42bd6c14afb2f1fcd8b92f0b93472906c44eb
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-10 20:06:02 +0100

    5.9.7

---
+++ catalog/5.9.7/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.9.7-050907
+sudo apt remove linux-image-unsigned-5.9.7-050907-generic 
+sudo apt remove linux-modules-5.9.7-050907-generic
+echo linux 5.9.7 is successfully removed
