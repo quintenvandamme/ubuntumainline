3205346fca0832b5012f0eeca6c503e9c1717452
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-11 18:12:06 +0100

    5.9.8

--- arm64.sh
+++ arm64.sh
@@ -1,9 +1,9 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.9.7
-cd /tmp/ubuntukernel5.9.7
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.7/arm64/linux-headers-5.9.7-050907-generic_5.9.7-050907.202011101240_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.7/arm64/linux-image-5.9.7-050907-generic_5.9.7-050907.202011101240_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.7/arm64/linux-modules-5.9.7-050907-generic_5.9.7-050907.202011101240_arm64.deb
+mkdir /tmp/ubuntukernel5.9.8
+cd /tmp/ubuntukernel5.9.8
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.8/arm64/linux-headers-5.9.8-050908-generic_5.9.8-050908.202011101634_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.8/arm64/linux-image-5.9.8-050908-generic_5.9.8-050908.202011101634_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.9.8/arm64/linux-modules-5.9.8-050908-generic_5.9.8-050908.202011101634_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.9.7
+rm -r /tmp/ubuntukernel5.9.8
