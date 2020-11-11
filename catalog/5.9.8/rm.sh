2cb5a373ad4f521772ae4747dc6eada87fd7b44f
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-11 18:14:44 +0100

    5.9.8

---
+++ catalog/5.9.8/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.9.8-050908
+sudo apt remove linux-image-unsigned-5.9.8-050908-generic 
+sudo apt remove linux-modules-5.9.8-050908-generic
+echo linux 5.9.8 is successfully removed
