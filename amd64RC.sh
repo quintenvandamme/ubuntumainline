8da8ac1ee861ad97f530e101f25fa2a454805a67
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-16 13:07:05 +0100

    5.10-rc4

--- amd64RC.sh
+++ amd64RC.sh
@@ -1,10 +1,10 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.10-rc3
-cd /tmp/ubuntukernel5.10-rc3
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc3/amd64/linux-headers-5.10.0-051000rc3-generic_5.10.0-051000rc3.202011082030_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc3/amd64/linux-headers-5.10.0-051000rc3_5.10.0-051000rc3.202011082030_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc3/amd64/linux-image-unsigned-5.10.0-051000rc3-generic_5.10.0-051000rc3.202011082030_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc3/amd64/linux-modules-5.10.0-051000rc3-generic_5.10.0-051000rc3.202011082030_amd64.deb
+mkdir /tmp/ubuntukernel5.10-rc4
+cd /tmp/ubuntukernel5.10-rc4
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc4/amd64/linux-headers-5.10.0-051000rc4-generic_5.10.0-051000rc4.202011152030_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc4/amd64/linux-headers-5.10.0-051000rc4_5.10.0-051000rc4.202011152030_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc4/amd64/linux-image-unsigned-5.10.0-051000rc4-generic_5.10.0-051000rc4.202011152030_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc4/amd64/linux-modules-5.10.0-051000rc4-generic_5.10.0-051000rc4.202011152030_amd64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.10-rc3
+rm -r /tmp/ubuntukernel5.10-rc4
