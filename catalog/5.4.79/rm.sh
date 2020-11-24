7d412b268b18a5e80f3f44551227812ebabf89fd
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-24 20:30:38 +0100

    5.4.79

---
+++ catalog/5.4.79/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.4.79-050479
+sudo apt remove linux-image-unsigned-5.4.79-050479-generic
+sudo apt remove linux-modules-5.4.79-050479-generic
+echo linux 5.4.79 is successfully removed
