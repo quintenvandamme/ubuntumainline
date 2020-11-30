4bc5198975ec1197376b12f115e1cce0d0209c16
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-30 07:02:31 +0000

    5.10-rc6

--- arm64RC.sh
+++ arm64RC.sh
@@ -1,9 +1,9 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.10-rc5
-cd /tmp/ubuntukernel5.10-rc5
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc5/arm64/linux-headers-5.10.0-051000rc5-generic_5.10.0-051000rc5.202011221956_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc5/arm64/linux-image-5.10.0-051000rc5-generic_5.10.0-051000rc5.202011221956_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc5/arm64/linux-modules-5.10.0-051000rc5-generic_5.10.0-051000rc5.202011221956_arm64.deb
+mkdir /tmp/ubuntukernel5.10-rc6
+cd /tmp/ubuntukernel5.10-rc6
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc6/arm64/linux-headers-5.10.0-051000rc6-generic_5.10.0-051000rc6.202011291930_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc6/arm64/linux-image-5.10.0-051000rc6-generic_5.10.0-051000rc6.202011291930_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10-rc6/arm64/linux-modules-5.10.0-051000rc6-generic_5.10.0-051000rc6.202011291930_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.10-rc5
+rm -r /tmp/ubuntukernel5.10-rc6
