c50ad38c64644830e9911383013b2aeb52726f44
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-11-30 07:03:58 +0000

    5.10-rc6

---
+++ catalog/5.10-rc6/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.10.0-051000rc6
+sudo apt remove linux-image-unsigned-5.10.0-051000rc6-generic
+sudo apt remove linux-modules-5.10.0-051000rc6-generic
+echo linux 5.10-rc6 is successfully removed
