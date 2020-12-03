22517a9eb5d2a54d3a56864c9b89a8d745a7749c
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-12-03 09:35:24 +0000

    5.9.12

---
+++ catalog/5.9.12/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.9.12-0509012
+sudo apt remove linux-image-unsigned-5.9.12-0509012-generic 
+sudo apt remove linux-modules-5.9.12-0509012-generic
+echo linux 5.9.12 is successfully removed
