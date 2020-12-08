074aa4d7e96e51b193f667c33e68dc4e49a37714
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-12-08 13:50:54 +0000

    5.4.82

--- arm64LTS.sh
+++ arm64LTS.sh
@@ -1,9 +1,9 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.4.81
-cd /tmp/ubuntukernel5.4.81
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.81/arm64/linux-headers-5.4.81-050481-generic_5.4.81-050481.202012020837_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.81/arm64/linux-image-unsigned-5.4.81-050481-generic_5.4.81-050481.202012020837_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.81/arm64/linux-modules-5.4.81-050481-generic_5.4.81-050481.202012020837_arm64.deb
+mkdir /tmp/ubuntukernel5.4.82
+cd /tmp/ubuntukernel5.4.82
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.82/arm64/linux-headers-5.4.82-050482-generic_5.4.82-050482.202012080534_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.82/arm64/linux-image-unsigned-5.4.82-050482-generic_5.4.82-050482.202012080534_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.82/arm64/linux-modules-5.4.82-050482-generic_5.4.82-050482.202012080534_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.4.81
+rm -r /tmp/ubuntukernel5.4.82
