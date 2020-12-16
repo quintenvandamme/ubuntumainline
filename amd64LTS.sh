6ebd015d7e8a822ddea8be12d3ff635f67a93ba0
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-12-16 11:27:00 +0000

    5.10.1

--- amd64LTS.sh
+++ amd64LTS.sh
@@ -1,10 +1,10 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.4.83
-cd /tmp/ubuntukernel5.4.83
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.83/amd64/linux-headers-5.4.83-050483-generic_5.4.83-050483.202012111336_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.83/amd64/linux-headers-5.4.83-050483_5.4.83-050483.202012111336_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.83/amd64/linux-image-unsigned-5.4.83-050483-generic_5.4.83-050483.202012111336_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.83/amd64/linux-modules-5.4.83-050483-generic_5.4.83-050483.202012111336_amd64.deb
+mkdir /tmp/ubuntukernel5.10.1
+cd /tmp/ubuntukernel5.10.1
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.1/amd64/linux-headers-5.10.1-051001-generic_5.10.1-051001.202012142031_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.1/amd64/linux-headers-5.10.1-051001_5.10.1-051001.202012142031_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.1/amd64/linux-image-unsigned-5.10.1-051001-generic_5.10.1-051001.202012142031_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.1/amd64/linux-modules-5.10.1-051001-generic_5.10.1-051001.202012142031_amd64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.4.83
+rm -r /tmp/ubuntukernel5.10.1
