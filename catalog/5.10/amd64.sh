404847ea140ae7fc754febbe114f0912808beb55
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-12-16 11:21:50 +0000

    5.10

---
+++ catalog/5.10/amd64.sh
@@ -0,0 +1,10 @@
+# downloads linux and linux headers
+mkdir /tmp/ubuntukernel5.10
+cd /tmp/ubuntukernel5.10
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-headers-5.10.0-051000-generic_5.10.0-051000.202012132330_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-headers-5.10.0-051000_5.10.0-051000.202012132330_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-image-unsigned-5.10.0-051000-generic_5.10.0-051000.202012132330_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10/amd64/linux-modules-5.10.0-051000-generic_5.10.0-051000.202012132330_amd64.deb
+sudo dpkg -i *.deb
+cd
+rm -r /tmp/ubuntukernel5.10
