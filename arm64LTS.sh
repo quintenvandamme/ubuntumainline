ff5b60701cde1f0ab75c71f4674315bbbfe37732
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-12-27 10:43:58 +0100

    5.10.3

--- arm64LTS.sh
+++ arm64LTS.sh
@@ -1,9 +1,9 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.10.2
-cd /tmp/ubuntukernel5.10.2
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.2/arm64/linux-headers-5.10.2-051002-generic_5.10.2-051002.202012210832_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.2/arm64/linux-image-5.10.2-051002-generic_5.10.2-051002.202012210832_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.2/arm64/linux-modules-5.10.2-051002-generic_5.10.2-051002.202012210832_arm64.deb
+mkdir /tmp/ubuntukernel5.10.3
+cd /tmp/ubuntukernel5.10.3
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/arm64/linux-headers-5.10.3-051003-generic_5.10.3-051003.202012261630_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/arm64/linux-image-5.10.3-051003-generic_5.10.3-051003.202012261630_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.3/arm64/linux-modules-5.10.3-051003-generic_5.10.3-051003.202012261630_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.10.2
+rm -r /tmp/ubuntukernel5.10.3
