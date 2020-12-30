d078c4cfa84578e8095bc8edb31d934b4df69df7
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2020-12-30 20:21:13 +0000

    5.10.4

---
+++ catalog/5.10.4/amd64.sh
@@ -0,0 +1,10 @@
+# downloads linux and linux headers
+mkdir /tmp/ubuntukernel5.10.4
+cd /tmp/ubuntukernel5.10.4
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/amd64/linux-headers-5.10.4-051004-generic_5.10.4-051004.202012301142_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/amd64/linux-headers-5.10.4-051004_5.10.4-051004.202012301142_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/amd64/linux-image-unsigned-5.10.4-051004-generic_5.10.4-051004.202012301142_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/amd64/linux-modules-5.10.4-051004-generic_5.10.4-051004.202012301142_amd64.deb
+sudo dpkg -i *.deb
+cd
+rm -r /tmp/ubuntukernel5.10.4
