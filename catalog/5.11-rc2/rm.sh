21fb0186388f121bc217b03f910bf1dbea9360f5
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-01-04 16:54:31 +0000

    5.11-rc2

---
+++ catalog/5.11-rc2/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.11.0-051100rc2
+sudo apt remove linux-image-unsigned-5.11.0-051100rc2-generic
+sudo apt remove linux-modules-5.11.0-051100rc2-generic
+echo linux 5.11-rc2 is successfully removed
