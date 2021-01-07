d4264ff24045597f5039465237a191cf83dcf2aa
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-01-07 18:42:37 +0000

    5.10.5

--- amd64.sh
+++ amd64.sh
@@ -1,10 +1,10 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.10.4
-cd /tmp/ubuntukernel5.10.4
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/amd64/linux-headers-5.10.4-051004-generic_5.10.4-051004.202012301142_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/amd64/linux-headers-5.10.4-051004_5.10.4-051004.202012301142_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/amd64/linux-image-unsigned-5.10.4-051004-generic_5.10.4-051004.202012301142_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/amd64/linux-modules-5.10.4-051004-generic_5.10.4-051004.202012301142_amd64.deb
+mkdir /tmp/ubuntukernel5.10.5
+cd /tmp/ubuntukernel5.10.5
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/amd64/linux-headers-5.10.5-051005-generic_5.10.5-051005.202101061537_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/amd64/linux-headers-5.10.5-051005_5.10.5-051005.202101061537_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/amd64/linux-image-unsigned-5.10.5-051005-generic_5.10.5-051005.202101061537_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/amd64/linux-modules-5.10.5-051005-generic_5.10.5-051005.202101061537_amd64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.10.4
+rm -r /tmp/ubuntukernel5.10.5
