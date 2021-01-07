e413eec9694b0b174cebf7bb6c480a6c3781b05f
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-01-07 18:44:04 +0000

    5.10.5

--- arm64.sh
+++ arm64.sh
@@ -1,9 +1,9 @@
 # downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.10.4
-cd /tmp/ubuntukernel5.10.4
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/arm64/linux-headers-5.10.4-051004-generic_5.10.4-051004.202012301142_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/arm64/linux-image-unsigned-5.10.4-051004-generic_5.10.4-051004.202012301142_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.4/arm64/linux-modules-5.10.4-051004-generic_5.10.4-051004.202012301142_arm64.deb
+mkdir /tmp/ubuntukernel5.10.5
+cd /tmp/ubuntukernel5.10.5
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/arm64/linux-headers-5.10.5-051005-generic_5.10.5-051005.202101061537_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/arm64/linux-image-5.10.5-051005-generic_5.10.5-051005.202101061537_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/arm64/linux-modules-5.10.5-051005-generic_5.10.5-051005.202101061537_arm64.deb
 sudo dpkg -i *.deb
 cd
-rm -r /tmp/ubuntukernel5.10.4
+rm -r /tmp/ubuntukernel5.10.5
