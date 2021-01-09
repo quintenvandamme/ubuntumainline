dcde91a39d92f6380752d9e21d8b0fb2ebe721f4
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-01-09 14:15:20 +0000

    Delete arm64.sh

--- arm64.sh
+++
@@ -1,9 +0,0 @@
-# downloads linux and linux headers
-mkdir /tmp/ubuntukernel5.10.5
-cd /tmp/ubuntukernel5.10.5
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/arm64/linux-headers-5.10.5-051005-generic_5.10.5-051005.202101061537_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/arm64/linux-image-5.10.5-051005-generic_5.10.5-051005.202101061537_arm64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.5/arm64/linux-modules-5.10.5-051005-generic_5.10.5-051005.202101061537_arm64.deb
-sudo dpkg -i *.deb
-cd
-rm -r /tmp/ubuntukernel5.10.5
