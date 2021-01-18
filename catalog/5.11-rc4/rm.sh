3ab7777fa7a0a71193f851955e5e38477c51a379
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-01-18 20:09:03 +0100

    5.11-rc4

---
+++ catalog/5.11-rc4/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.11.0-051100rc4
+sudo apt remove linux-image-unsigned-5.11.0-051100rc4-generic
+sudo apt remove linux-modules-5.11.0-051100rc4-generic
+echo linux 5.11-rc4 is successfully removed
