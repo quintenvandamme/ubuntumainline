d0d189857fc8ba34d09b28d13cdb3123e0741038
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-01-20 13:02:22 +0100

    5.4.91

---
+++ catalog/5.4.91/amd64LTS.sh
@@ -0,0 +1,27 @@
+# linux 5.4.91
+ 
+## amd64
+
+### install
+```bash
+curl -s https://raw.githubusercontent.com/HexaOneOfficial/ubuntumainline/main/catalog/5.4.91/amd64LTS.sh | sh
+``` 
+### remove
+```bash
+wget https://raw.githubusercontent.com/HexaOneOfficial/ubuntumainline/main/catalog/5.4.91/rm.sh
+sudo chmod +x rm.sh && sudo ./rm.sh
+```
+## arm64
+
+### install
+```bash
+curl -s https://raw.githubusercontent.com/HexaOneOfficial/ubuntumainline/main/catalog/5.4.91/arm64LTS.sh | sh
+``` 
+### remove
+```bash
+wget https://raw.githubusercontent.com/HexaOneOfficial/ubuntumainline/main/catalog/5.4.91/rm.sh
+sudo chmod +x rm.sh && sudo ./rm.sh
+``` 
+ 
+ 
+kernel by [`https://kernel.ubuntu.com`](https://kernel.ubuntu.com/)
