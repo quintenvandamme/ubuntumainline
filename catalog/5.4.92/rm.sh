9103b9934eb69cf9a16a7ec2aeb616e9f96389a9
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-01-23 18:44:31 +0100

    5.4.92

---
+++ catalog/5.4.92/rm.sh
@@ -0,0 +1,7 @@
+
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.4.92-050492
+sudo apt remove linux-image-unsigned-5.4.92-050492-generic
+sudo apt remove linux-modules-5.4.92-050492-generic
+echo linux 5.4.92 is successfully removed
