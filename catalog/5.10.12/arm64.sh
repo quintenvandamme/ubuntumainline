05d008feb7565424a52675b18fe6a101eea18fa7
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-01-30 15:33:01 +0000

    5.10.12

---
+++ catalog/5.10.12/arm64.sh
@@ -0,0 +1,9 @@
+# downloads linux and linux headers
+mkdir /tmp/ubuntukernel5.10.12
+cd /tmp/ubuntukernel5.10.12
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.12/arm64/linux-headers-5.10.12-051012-generic_5.10.12-051012.202101301330_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.12/arm64/linux-image-5.10.12-051012-generic_5.10.12-051012.202101301330_arm64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.10.12/arm64/linux-modules-5.10.12-051012-generic_5.10.12-051012.202101301330_arm64.deb
+sudo dpkg -i *.deb
+cd
+rm -r /tmp/ubuntukernel5.10.12
