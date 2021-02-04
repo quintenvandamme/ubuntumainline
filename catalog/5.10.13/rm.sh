2a5d52e16b76ac85496b2273788f9002af895f21
Quinten <58103738+HexaOneOfficial@users.noreply.github.com>
2021-02-04 07:51:50 +0000

    5.10.13

---
+++ catalog/5.10.13/rm.sh
@@ -0,0 +1,6 @@
+echo only remove kernel if you have a newer one!
+sleep 2
+sudo apt remove linux-headers-5.10.13-051013
+sudo apt remove linux-image-unsigned-5.10.13-051013-generic 
+sudo apt remove linux-modules-5.10.13-051013-generic
+echo linux 5.10.13 is successfully removed
