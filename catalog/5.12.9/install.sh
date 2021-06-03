53c9dd0d2e6aca1c8f2b3e9676fdec9c83fea8f4
hexa-one <oproo48ly@relay.firefox.com>
2021-06-03 20:33:21 +0000

    5.12.9

---
+++ catalog/5.12.9/install.sh
@@ -0,0 +1,53 @@
+#! /usr/bin/env bash
+
+KERNEL_VER="5.12.9"
+VER_STR="051209"
+
+while [[ $# -gt 0 ]]; do
+  PROG_ARGS+=("${1}")
+  case "${1}" in
+    -amd|--amd64)
+      mkdir /tmp/ubuntukernel$KERNEL_VER
+      cd /tmp/ubuntukernel$KERNEL_VER
+      wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/amd64/linux-headers-5.12.9-051209-generic_5.12.9-051209.202106030850_amd64.deb
+      wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/amd64/linux-headers-5.12.9-051209_5.12.9-051209.202106030850_all.deb
+      wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/amd64/linux-image-unsigned-5.12.9-051209-generic_5.12.9-051209.202106030850_amd64.deb
+      wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/amd64/linux-modules-5.12.9-051209-generic_5.12.9-051209.202106030850_amd64.deb
+      sudo dpkg -i *.deb
+      cd
+      rm -r /tmp/ubuntukernel$KERNEL_VER
+      if [ -f "/boot/initrd.img-$KERNEL_VER-$VER_STR-generic" ] 
+      then
+          echo linux $KERNEL_VER is successfully installed!
+      else
+          echo an error occurred while installing
+      fi
+      break
+      ;;
+    -arm|--arm64)
+      mkdir /tmp/ubuntukernel$KERNEL_VER
+      cd /tmp/ubuntukernel$KERNEL_VER
+      wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/arm64/linux-headers-5.12.9-051209-generic_5.12.9-051209.202106030850_arm64.deb
+      wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/arm64/linux-image-unsigned-5.12.9-051209-generic_5.12.9-051209.202106030850_arm64.deb
+      wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.12.9/arm64/linux-modules-5.12.9-051209-generic_5.12.9-051209.202106030850_arm64.deb
+      sudo dpkg -i *.deb
+      cd
+      rm -r /tmp/ubuntukernel$KERNEL_VER
+      break
+      ;;
+    -r|--remove)
+      echo only remove kernel if you have a newer one!
+      sleep 2
+      sudo apt remove linux-headers-$KERNEL_VER-$VER_STR
+      sudo apt remove linux-image-unsigned-$KERNEL_VER-$VER_STR-generic 
+      sudo apt remove linux-modules-$KERNEL_VER-$VER_STR-generic
+      if [ -f "/boot/initrd.img-$KERNEL_VER-$VER_STR-generic" ] 
+      then
+          echo linux $KERNEL_VER is successfully removed!
+      else
+          echo an error occurred while removing linux $KERNEL_VER
+      fi
+      break
+      ;;
+        esac
+done
--- readme.md
+++ readme.md
@@ -21,10 +21,10 @@
 
 ### amd64
 
-**mainline kernel 5.12.8**
+**mainline kernel 5.12.9**
 
 ```bash
-cd /tmp/ && wget https://raw.githubusercontent.com/hexa-one/ubuntumainline/main/catalog/5.12.8/install.sh && chmod +x install.sh && sudo ./install.sh -amd
+cd /tmp/ && wget https://raw.githubusercontent.com/hexa-one/ubuntumainline/main/catalog/5.12.9/install.sh && chmod +x install.sh && sudo ./install.sh -amd
 ```
 **lts kernel 5.10.35**
 
@@ -39,9 +39,9 @@ cd /tmp/ && wget https://raw.githubusercontent.com/hexa-one/ubuntumainline/main/
 
 ### arm64
 
-**mainline kernel 5.12.8**
+**mainline kernel 5.12.9**
 ```bash
-cd /tmp/ && wget https://raw.githubusercontent.com/hexa-one/ubuntumainline/main/catalog/5.12.8/install.sh && chmod +x install.sh && sudo ./install.sh -arm
+cd /tmp/ && wget https://raw.githubusercontent.com/hexa-one/ubuntumainline/main/catalog/5.12.9/install.sh && chmod +x install.sh && sudo ./install.sh -arm
 ```
 
 **lts kernel 5.10.35**
